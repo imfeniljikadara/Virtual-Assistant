a006157b2af507710808d62e79b8f0a6ecba5d3d
Fenil Jikadara <146850675+imfeniljikadara@users.noreply.github.com>
2024-07-25 12:21:42 +0530

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,91 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx4
+import musicLibrary
+import requests
+from openai import OpenAI
+from gtts import gTTS
+import pygame
+import os
+from transformers import pipeline
+
+recognizer = sr.Recognizer()
+engine = pyttsx4.init() 
+newsapi = "<Your Key Here>"
+
+def speak_old(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def speak(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3') 
+    pygame.mixer.init()
+    pygame.mixer.music.load('temp.mp3')
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    pygame.mixer.music.unload()
+    os.remove("temp.mp3") 
+
+def aiProcess(command):
+    client = OpenAI(api_key="<Your Key Here>",
+    )
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud. Give short responses please"},
+        {"role": "user", "content": command}
+    ]
+    )
+    return completion.choices[0].message.content
+
+def aiProcess(command):
+    generator = pipeline('text-generation', model='gpt-2')
+    result = generator(command, max_length=50, num_return_sequences=1)
+    return result[0]['generated_text']
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+    elif "news" in c.lower():
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey={newsapi}")
+        if r.status_code == 200:
+            data = r.json()
+            articles = data.get('articles', [])
+            for article in articles:
+                speak(article['title'])
+    else:
+        output = aiProcess(c)
+        speak(output) 
+
+
+if __name__ == "__main__":
+    speak("Initializing Jarvis....")
+    while True:
+        r = sr.Recognizer()
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Ya")
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+                    processCommand(command)
+        except Exception as e:
+            print("Error; {0}".format(e))
